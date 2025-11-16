469ed88f4e4bd0385fe80d8e9aae06ee88acf275
foszlany <radojcickilienborisz@gmail.com>
2025-11-16 15:12:34 +0100

    Init KanaMenu

---
+++ Hazi/JapaneseStudyTool/JapaneseStudyTool.UI/KanaMenu.cs
@@ -0,0 +1,27 @@
+﻿namespace JapaneseStudyTool.JapaneseStudyTool.UI
+{
+    internal class KanaMenu
+    {
+        public static void RunKanaMenu()
+        {
+            Console.Clear();
+
+            while (true)
+            {
+                Console.WriteLine("Welcome to Kana practice!");
+                Console.WriteLine("With this module, you can practice reading hiragana, katakana and both!\n");
+                Console.WriteLine("To start, enter the desired difficulty and the mode using the following keywords:");
+                Console.WriteLine("easy | medium | hard");
+                Console.WriteLine("hiragana | katakana | mixed\n");
+                Console.WriteLine("Example: 'easy hiragana'");
+
+                Console.WriteLine("Alternatively, type 'exit' to go back!");
+
+                Console.Write(">");
+                string expression = Console.ReadLine() ?? string.Empty;
+
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ Hazi/JapaneseStudyTool/JapaneseStudyTool.UI/MainMenu.cs
@@ -0,0 +1,47 @@
+﻿using JapaneseStudyTool.JapaneseStudyTool.Core.Enum;
+
+namespace JapaneseStudyTool.JapaneseStudyTool.UI
+{
+    internal class MainMenu
+    {
+        public static void RunMainMenu()
+        {
+            Console.Clear();
+
+            while (true)
+            {
+                Console.WriteLine("Welcome to JapaneseStudyTool! Select your desired mode.");
+                Console.WriteLine("[1] Kana practice");
+                Console.WriteLine("[2] Vocabulary practice");
+                Console.WriteLine("[3] Exit");
+
+                Console.Write(">");
+                string expression = Console.ReadLine() ?? string.Empty;
+
+                if (Int32.TryParse(expression, out int modeInt) && Enum.IsDefined(typeof(MainMenuMode), modeInt))
+                {
+                    MainMenuMode mode = (MainMenuMode)modeInt;
+
+                    switch (mode)
+                    {
+                        case MainMenuMode.KanaPractice:
+                            KanaMenu.RunKanaMenu();
+                            break;
+
+                        case MainMenuMode.VocabularyPractice:
+                            throw new NotImplementedException();
+
+                        case MainMenuMode.Exit:
+                            Environment.Exit(0);
+                            return;
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid input.\n");
+                }
+            }
+        }
+    }
+}
