737c34ce96dc2c0698cb8c69d8368e572bb995ff
foszlany <radojcickilienborisz@gmail.com>
2025-11-16 14:46:51 +0100

    Init main menu

---
+++ Hazi/JapaneseStudyTool/JapaneseStudyTool.Core/Enum/MainMenuMode.cs
@@ -0,0 +1,9 @@
+﻿namespace JapaneseStudyTool.JapaneseStudyTool.Core.Enum
+{
+    enum MainMenuMode
+    {
+        KanaPractice = 1,
+        VocabularyPractice = 2,
+        Exit = 3
+    }
+}
--- Hazi/JapaneseStudyTool/JapaneseStudyTool/Program.cs
+++ Hazi/JapaneseStudyTool/JapaneseStudyTool/Program.cs
@@ -1,7 +1,41 @@
-﻿internal class Program
+﻿using JapaneseStudyTool.JapaneseStudyTool.Core.Enum;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        while (true)
+        {
+            Console.WriteLine("Welcome to JapaneseStudyTool! Select your desired mode.");
+            Console.WriteLine("[1] Kana practice");
+            Console.WriteLine("[2] Vocabulary practice");
+            Console.WriteLine("[3] Exit");
+
+            Console.Write(">");
+            string expression = Console.ReadLine() ?? string.Empty;
+
+            if (Int32.TryParse(expression, out int modeInt) && Enum.IsDefined(typeof(MainMenuMode), modeInt))
+            {
+                MainMenuMode mode = (MainMenuMode)modeInt;
+
+                switch (mode)
+                {
+                    case MainMenuMode.KanaPractice:
+                        throw new NotImplementedException();
+
+                    case MainMenuMode.VocabularyPractice:
+                        throw new NotImplementedException();
+
+                    case MainMenuMode.Exit:
+                        Environment.Exit(0);
+                        return;
+                }
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Invalid input.\n");
+            }
+        }
     }
 }
